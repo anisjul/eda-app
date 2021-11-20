88e9e83befc87564754b1346309cc000205945ba
anisjul <94744584+anisjul@users.noreply.github.com>
2021-11-20 20:30:16 +0800

    Update eda.py

--- eda.py
+++ eda.py
@@ -33,7 +33,7 @@ if uploaded_file is not None:
     st.header('**Pandas Profiling Report**')
     st_profile_report(pr)
     profile = df.profile_report(title ='Pandas Profiling Report')
-    profile.to_file(output_file="output.htm")
+    profile.to_file(output_file="output.html")
 else:
     st.info('Awaiting for CSV file to be uploaded.')
     if st.button('Press to use Example Dataset'):
